97ebef5a21e81fb0207c752a309aa8cbc2abf09a
Mezen <jebali.mazen@gmail.com>
2024-10-29 20:14:27 +0100

    fix cors

--- backend/Gs-Data/com/src/main/java/Gs_Data/project/com/Controllers/RessourceController.java
+++ backend/Gs-Data/com/src/main/java/Gs_Data/project/com/Controllers/RessourceController.java
@@ -15,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/ressources")
 public class RessourceController {
